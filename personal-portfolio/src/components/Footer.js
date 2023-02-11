13c64e4c4f6974be7e7946b42cfbaaa1259603ad
@kirui <kirui@asilimia.co.ke>
2023-02-11 11:52:16 +0300

    fixed issues that causes the app to crash

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -2,8 +2,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import app from '../assets/img/app.png'
+import email from '../assets/img/email.jpg'
 
 export const Footer = () => {
   return (
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/vincent-kirui-b814101a2/"><img src={navIcon1} alt="" /></a>
+            <a href=" https://wa.me/254799476072"><img src={app} alt="" /></a>
+            <a href="mailto:kiruivinie1@gmail.com"><img src={email} alt="" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -3,8 +3,6 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -38,27 +36,27 @@ export const Skills = () => {
                         <p>Python | Django | JavaScript/Typescript | PostgreSQL | Docker | SQL | Firebase |Data analysis | Software tesing (mobile and web) | Git | Cloud Functions | basic deployment | Machine learning | CRUD Applications |Agile/scrum | Statistical Modelling |Customer Service & Relationship Management | USSD | Analytical Skills | Information Flow Management | Research & Development | Data Management | Team Player | Good Communication & Interpersonal Skills | Jira and TestRail  test management tools</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter1} alt="" />
                                 <h5>Backend Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter2} alt="" />
                                 <h5>Front Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Data Engineering</h5>
+                                <img src={meter3} alt="" />
+                                <h5>Data Engineering/Science</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Cloud services</h5>
+                                <img src={meter1} alt="" />
+                                <h5>Machine Learning</h5>
                             </div>
                         </Carousel>
                     </div>
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        <img className="background-image-left" src={colorSharp} alt="" />
     </section>
   )
 }
